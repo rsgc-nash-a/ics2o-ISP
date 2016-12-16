552abc404c04c2ec335ab4b90223a38cc1435cd0
Aaron Nash <emailaaron@rogers.com>
2016-12-16 10:34:13 -0500

    I have now given the areaUIPickerView brains in order for her to be able to select which unit the person would like to select, and return it in a UI text field. To do: Implement it for each storyboard.

--- AreaViewController.swift
+++ AreaViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class AreaViewController: UIViewController {
+class AreaViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,9 +19,28 @@ class AreaViewController: UIViewController {
     @IBOutlet weak var areaPicker: UIPickerView!
     
     @IBOutlet weak var areaOutput: UILabel!
+
+    //This is the data source for the UI Picker View
+    let areaPickerData = ["uno", "dos", "tres"]
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Delegets and Data
+    // MARK: - Data sources
+    // this tells the UI Picker view how many spinning parts to show
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        //we only need one so return, 
+        return 1
+    }
+    // Tell the UI Picker View how many rows to show
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // we are now getting it to return the number of options that we had in areaPickerData
+        return areaPickerData.count
+        
     }
     
 
