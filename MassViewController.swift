122979c44010d0a5dc38413e6be98464f6f4a4c8
Aaron Nash <emailaaron@rogers.com>
2016-12-27 10:14:59 -0400

    I now have created a ViewController for Mass, and a Cocoa touch class file. To do: write the code for the cocoa touch class file

---
+++ MassViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MassViewController.swift
+//  Unit Converter
+//
+//  Created by Aaron Nash on 2016-12-27.
+//  Copyright © 2016 Aaron Nash. All rights reserved.
+//
+
+import UIKit
+
+class MassViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
